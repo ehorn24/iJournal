eeb5c83e5b513f74aecb237b6f8b02d1760e065c
Ella Horn <ellalouisahorn@yahoo.com>
2020-08-02 20:42:47 -0500

    created mock data

---
+++ src/Mock Data/MockData.js
@@ -0,0 +1,34 @@
+const MockData = [
+  {
+    journal_title: "High School",
+    date_created: "8/8/2010",
+    entries: [
+      "First day of school!",
+      "I hate homework",
+      "I'm in love with Timmy",
+      "Christmas break",
+    ],
+  },
+  {
+    journal_title: "College",
+    date_created: "07/12/2015",
+    entries: [
+      "I got into my top school!!",
+      "Joined a sorority",
+      "Got kicked out of my sorority...",
+      "Spring breaaaakkkk",
+    ],
+  },
+  {
+    journal_title: "Quarantine Diaries",
+    date_created: "02/25/2020",
+    entries: [
+      "COVID sucks!!",
+      "I miss my friends :(",
+      "I can't stop eating ice cream",
+      "WHEN WILL IT END",
+    ],
+  },
+];
+
+module.export = MockData;
