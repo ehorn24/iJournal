b79b50575bc33baedf48c59c571ae7a10bb7cbc0
Ella Horn <ellalouisahorn@yahoo.com>
2020-08-10 21:08:57 -0500

    change data format

--- src/Mock Data/MockEntries.js
+++ src/Mock Data/MockEntries.js
@@ -1,8 +1,9 @@
-const MockEntries = [
+export const MockEntries = [
   {
     journal: "High School",
     entry_title: "First day of school!",
-    entry_date: "8/8/2010",
+    entry_date: "August 8",
+    entry_year: 2010,
     tags: ["high school", "first day", "nervous"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -10,7 +11,8 @@ const MockEntries = [
   {
     journal: "High School",
     entry_title: "I hate homework",
-    entry_date: "9/1/2010",
+    entry_date: "September 1",
+    entry_year: 2010,
     tags: ["high school", "homework", "boring"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -18,7 +20,8 @@ const MockEntries = [
   {
     journal: "High School",
     entry_title: "I'm in love with Timmy",
-    entry_date: "9/23/2010",
+    entry_date: "September 23",
+    entry_year: 2010,
     tags: ["boys", "timmy", "love"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -26,7 +29,8 @@ const MockEntries = [
   {
     journal: "High School",
     entry_title: "Christmas break",
-    entry_date: "12/15/2010",
+    entry_date: "December 15",
+    entry_year: 2010,
     tags: ["christmas", "holidays", "family"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -34,7 +38,8 @@ const MockEntries = [
   {
     journal: "College",
     entry_title: "I got into my top school!!",
-    entry_date: "07/15/2015",
+    entry_date: "July 15",
+    entry_year: 2015,
     tags: ["college", "excited", "florida state"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -42,7 +47,8 @@ const MockEntries = [
   {
     journal: "College",
     entry_title: "Joined a sorority",
-    entry_date: "08/28/2015",
+    entry_date: "August 28",
+    entry_year: 2015,
     tags: ["sorority", "sisters", "florida state"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -50,7 +56,8 @@ const MockEntries = [
   {
     journal: "College",
     entry_title: "Got kicked out of my sorority...",
-    entry_date: "10/02/2015",
+    entry_date: "October 2",
+    entry_year: 2015,
     tags: ["sorority", "florida state", "sad"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -58,7 +65,8 @@ const MockEntries = [
   {
     journal: "College",
     entry_title: "Spring breaaaakkkk",
-    entry_date: "03/19/2016",
+    entry_date: "March 19",
+    entry_year: 2016,
     tags: ["college", "spring break", "florida state"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -66,7 +74,8 @@ const MockEntries = [
   {
     journal: "Quarantine Diaries",
     entry_title: "COVID sucks!!",
-    entry_date: "02/25/2020",
+    entry_date: "February 25",
+    entry_year: 2020,
     tags: ["covid", "quarantine", "pandemic"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -74,7 +83,8 @@ const MockEntries = [
   {
     journal: "Quarantine Diaries",
     entry_title: "I miss my friends :(",
-    entry_date: "03/04/2020",
+    entry_date: "March 4",
+    entry_year: 2020,
     tags: ["friends", "sad", "covid"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -82,7 +92,8 @@ const MockEntries = [
   {
     journal: "Quarantine Diaries",
     entry_title: "I can't stop eating ice cream",
-    entry_date: "03/24/2020",
+    entry_date: "March 24",
+    entry_year: 2020,
     tags: ["quarantine", "ice cream", "bored"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -90,11 +101,10 @@ const MockEntries = [
   {
     journal: "Quarantine Diaries",
     entry_title: "WHEN WILL IT END",
-    entry_date: "04/10/2020",
+    entry_date: "April 10",
+    entry_year: 2020,
     tags: ["covid", "bored"],
     entry_text:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 ];
-
-module.exports = MockEntries;
