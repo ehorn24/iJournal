35534af681f4d109d963a248736dd55a96e8ea8b
Ella Horn <ellalouisahorn@yahoo.com>
2020-08-10 20:51:13 -0500

    created sidebar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { MockData } from "../Mock Data/MockData";
+import TagsInput from "./TagsInput";
 
-const Home = () => {
+const Home = ({ addTags, removeTags, tags }) => {
   return (
     <>
       <header>
@@ -10,14 +11,44 @@ const Home = () => {
       </header>
       <main className="home-main">
         <section className="sidebar">
-          <h4>Sidebar Goes Here</h4>
+          <h4>Search for entries</h4>
+          <label htmlFor="filterby-journal">Journal:</label>
+          <select name="journal" id="journal">
+            <option value=""></option>
+            {MockData.map((j, i) => {
+              return (
+                <option value={j.journal_title} key={i}>
+                  {j.journal_title}
+                </option>
+              );
+            })}
+          </select>
+          <label htmlFor="filterby-month">Month</label>
+          <select name="month" id="month">
+            <option value=""></option>
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
+          <label htmlFor="filterby-tags">Tags:</label>
+          <TagsInput addTags={addTags} removeTags={removeTags} tags={tags} />
+          <button className="submit">Search</button>
         </section>
         <section className="my-journals">
           <h4>My Journals</h4>
           <div className="journals-flex">
-            {MockData.map((j) => {
+            {MockData.map((j, i) => {
               return (
-                <Link to={"/" + j.journal_title} className="journal">
+                <Link to={"/" + j.journal_title} className="journal" key={i}>
                   {j.journal_title}
                 </Link>
               );
