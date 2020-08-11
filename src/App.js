9c662200f09a64f5588d929332955c5515a5ba82
Ella Horn <ellalouisahorn@yahoo.com>
2020-08-10 20:51:27 -0500

    added tag input functions

--- src/App.js
+++ src/App.js
@@ -6,12 +6,45 @@ import Landing from "./Components/Landing";
 import Home from "./Components/Home";
 
 export default class App extends Component {
+  state = {
+    tags: [],
+  };
+
+  //Tags Input Functions
+  addTags = (e) => {
+    if (e.key === "Enter" && e.target.value !== "") {
+      this.setState({ tags: [...this.state.tags, e.target.value] });
+      e.target.value = "";
+    }
+  };
+
+  removeTags = (index) => {
+    this.setState({
+      tags: [
+        ...this.state.tags.filter(
+          (tag) => this.state.tags.indexOf(tag) !== index
+        ),
+      ],
+    });
+  };
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/" render={(props) => <Landing {...props} />} />
-          <Route exact path="/home" render={(props) => <Home {...props} />} />
+          <Route
+            exact
+            path="/home"
+            render={(props) => (
+              <Home
+                {...props}
+                addTags={this.addTags}
+                removeTags={this.removeTags}
+                tags={this.state.tags}
+              />
+            )}
+          />
         </Switch>
       </Router>
     );
