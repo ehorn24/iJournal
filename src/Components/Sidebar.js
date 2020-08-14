529cc29c5c76821c847db95d3aeabe6aaaf3dcb2
Ella Horn <ellalouisahorn@yahoo.com>
2020-08-13 20:13:53 -0500

    sidebar css

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -12,48 +12,64 @@ const Sidebar = ({ addTags, removeTags, tags, page }) => {
     case "Home":
       return (
         <div className="sidebar">
-          <h4>Search for entries</h4>
-          <label htmlFor="filterby-journal">Journal:</label>
-          <select name="journal" id="journal">
-            <option value=""></option>
-            {MockData.map((j, i) => {
-              return (
-                <option value={j.journal_title} key={i}>
-                  {j.journal_title}
-                </option>
-              );
-            })}
-          </select>
-          <label htmlFor="filterby-month">Month</label>
-          <select name="month" id="month">
-            <option value=""></option>
-            <option value="1">January</option>
-            <option value="2">February</option>
-            <option value="3">March</option>
-            <option value="4">April</option>
-            <option value="5">May</option>
-            <option value="6">June</option>
-            <option value="7">July</option>
-            <option value="8">August</option>
-            <option value="9">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
-          </select>
-          <label htmlFor="">Year:</label>
-          <select name="year" id="year">
-            <option value=""></option>
-            {years.map((y, i) => {
-              return (
-                <option key={i} value={y}>
-                  {y}
-                </option>
-              );
-            })}
-          </select>
-          <label htmlFor="filterby-tags">Tags:</label>
-          <TagsInput addTags={addTags} removeTags={removeTags} tags={tags} />
-          <button className="submit">Search</button>
+          <h4 className="sidebar-header">Search for journal entries</h4>
+          <form className="sidebar-form" onSubmit={(e) => e.preventDefault()}>
+            <ul>
+              <li>
+                <label htmlFor="filterby-journal">Journal:</label>
+                <select name="journal" id="journal">
+                  <option value=""></option>
+                  {MockData.map((j, i) => {
+                    return (
+                      <option value={j.journal_title} key={i}>
+                        {j.journal_title}
+                      </option>
+                    );
+                  })}
+                </select>
+              </li>
+              <li>
+                <label htmlFor="filterby-month">Month:</label>
+                <select name="month" id="month">
+                  <option value=""></option>
+                  <option value="1">January</option>
+                  <option value="2">February</option>
+                  <option value="3">March</option>
+                  <option value="4">April</option>
+                  <option value="5">May</option>
+                  <option value="6">June</option>
+                  <option value="7">July</option>
+                  <option value="8">August</option>
+                  <option value="9">September</option>
+                  <option value="10">October</option>
+                  <option value="11">November</option>
+                  <option value="12">December</option>
+                </select>
+              </li>
+              <li>
+                <label htmlFor="">Year:</label>
+                <select name="year" id="year">
+                  <option value=""></option>
+                  {years.map((y, i) => {
+                    return (
+                      <option key={i} value={y}>
+                        {y}
+                      </option>
+                    );
+                  })}
+                </select>
+              </li>
+              <li>
+                <label htmlFor="filterby-tags">Tags:</label>
+                <TagsInput
+                  addTags={addTags}
+                  removeTags={removeTags}
+                  tags={tags}
+                />
+              </li>
+            </ul>
+            <button className="sidebar-search">Search</button>
+          </form>
         </div>
       );
     default:
