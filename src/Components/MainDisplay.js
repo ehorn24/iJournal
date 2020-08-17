369b774e7c6e46a60147e03b77ff901562432000
Ella Horn <ellalouisahorn@yahoo.com>
2020-08-17 07:34:40 -0500

    display entries that match all filters

---
+++ src/Components/MainDisplay.js
@@ -0,0 +1,89 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { MockData } from "../Mock Data/MockData";
+import { MockEntries } from "../Mock Data/MockEntries";
+
+const MainDisplay = ({ journal, month, year, tags }) => {
+  //get selected filters
+  let currentFilters = {};
+  const getFilters = (journal, month, year, tags) => {
+    if (journal !== "") {
+      currentFilters.journal = journal;
+    }
+    if (month !== "") {
+      currentFilters.month = month;
+    }
+    if (year !== "") {
+      currentFilters.year = year;
+    }
+    if (tags.length !== 0) {
+      currentFilters.tags = tags;
+    }
+    return currentFilters;
+  };
+  getFilters(journal, month, year, tags);
+
+  const matchingEntries = MockEntries.filter((ent) => {
+    return Object.keys(currentFilters).every((k) => {
+      let bool = true;
+      if (typeof ent[k] === "string") {
+        bool = (ent[k] === currentFilters[k]) * bool;
+      } else {
+        let entrySet = new Set(ent.tags.map((t) => t));
+        let tagsSet = new Set(currentFilters[k]);
+        let intersect = new Set([...entrySet].filter((x) => tagsSet.has(x)));
+        bool = (intersect.size === tagsSet.size) * bool;
+      }
+      return bool;
+    });
+  });
+
+  if (Object.keys(currentFilters).length === 0) {
+    return (
+      <main className="home-main">
+        <section className="my-journals">
+          <h4>My Journals</h4>
+          <div className="journals-flex">
+            {MockData.map((j, i) => {
+              return (
+                <Link to={"/" + j.journal_title} className="journal" key={i}>
+                  {j.journal_title}
+                </Link>
+              );
+            })}
+          </div>
+        </section>
+      </main>
+    );
+  } else {
+    return (
+      <main className="home-main">
+        <section className="my-journals">
+          <h4>Looking for one of these?</h4>
+          <div>
+            {matchingEntries.length !== null
+              ? matchingEntries.map((m, i) => {
+                  return (
+                    <div key={i}>
+                      <h3>{m.entry_title}</h3>
+                      <p>
+                        {m.month} {m.date}, {m.year}
+                      </p>
+                      <p>
+                        Tags:{" "}
+                        {m.tags.map((t) => {
+                          return t + "/";
+                        })}
+                      </p>
+                    </div>
+                  );
+                })
+              : "Didn't work"}
+          </div>
+        </section>
+      </main>
+    );
+  }
+};
+
+export default MainDisplay;
