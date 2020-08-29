5a972e7e7d666d7ef55fb6670b374784e35d7fdf
Ella Horn <ellalouisahorn@yahoo.com>
2020-08-29 16:12:32 -0500

    linked header to homepage, css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <header className="ijournal-header">iJournal</header>;
+  return (
+    <header className="ijournal-header">
+      <Link to="/home">iJournal</Link>
+    </header>
+  );
 };
 
 export default Header;
