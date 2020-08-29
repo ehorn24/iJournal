86e571091f0b45176b2f970da7b416326bc0b330
Ella Horn <ellalouisahorn@yahoo.com>
2020-08-29 15:48:37 -0500

    edited sidebar for specific journal page

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -90,6 +90,67 @@ const Sidebar = ({
           </form>
         </div>
       );
+    case "Journal":
+      return (
+        <div className="sidebar">
+          <h4 className="sidebar-header">Search for journal entries</h4>
+          <form
+            className="sidebar-form"
+            onChange={handleFormChange}
+            onSubmit={(e) => e.preventDefault()}
+            onReset={clearFilters}
+          >
+            <ul>
+              <li>
+                <label htmlFor="filterby-month">Month:</label>
+                <select name="month" id="month">
+                  <option value=""></option>
+                  <option value="January">January</option>
+                  <option value="February">February</option>
+                  <option value="March">March</option>
+                  <option value="April">April</option>
+                  <option value="May">May</option>
+                  <option value="June">June</option>
+                  <option value="July">July</option>
+                  <option value="August">August</option>
+                  <option value="September">September</option>
+                  <option value="October">October</option>
+                  <option value="November">November</option>
+                  <option value="December">December</option>
+                </select>
+              </li>
+              <li>
+                <label htmlFor="">Year:</label>
+                <select name="year" id="year">
+                  <option value=""></option>
+                  {years.map((y, i) => {
+                    return (
+                      <option key={i} value={y}>
+                        {y}
+                      </option>
+                    );
+                  })}
+                </select>
+              </li>
+              <li>
+                <label htmlFor="filterby-tags">Tags:</label>
+                <TagsInput
+                  addTags={addTags}
+                  removeTags={removeTags}
+                  tags={tags}
+                />
+              </li>
+            </ul>
+            <div className="sidebar-button-flex">
+              <input
+                type="reset"
+                className="sidebar-button"
+                value="Clear All Filters"
+              />
+            </div>
+          </form>
+        </div>
+      );
     default:
       return page;
   }
