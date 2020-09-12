610d463edfed7392f72bfb6e35f44109bf22fd54
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-11 21:43:51 -0500

    get user journals/entries on mount, filters, main page

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 import { getUserInfo } from "../Services/userServices";
+import { getUserJournals } from "../Services/journalServices";
+import { getUserEntries } from "../Services/entryServices";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MainDisplay from "./MainDisplay";
 
 export default class Main extends Component {
   state = {
     username: "",
     userInfo: [],
+    journals: [],
+    entries: [],
+    tags: [],
+    journal: "",
+    month: "",
+    year: "",
   };
 
   componentDidMount() {
@@ -13,7 +25,18 @@ export default class Main extends Component {
         this.setState({ username: localStorage.getItem("username") }, () => {
           getUserInfo(this.state.username).then((res) => {
             if (!res.error) {
-              this.setState({ userInfo: res });
+              this.setState({ userInfo: res }, () => {
+                getUserJournals(this.state.userInfo.id).then((res) => {
+                  if (!res.error) {
+                    this.setState({ journals: res });
+                  }
+                });
+                getUserEntries(this.state.userInfo.id).then((res) => {
+                  if (!res.error) {
+                    this.setState({ entries: res });
+                  }
+                });
+              });
             }
           });
         });
@@ -21,11 +44,73 @@ export default class Main extends Component {
     }
   }
 
+  handleFormChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  //Sidebar functions
+  clearFilters = () => {
+    this.setState({
+      journal: "",
+      month: "",
+      year: "",
+      tags: [],
+    });
+  };
+
+  //Tags
+  addTags = (e) => {
+    if (e.key === "Enter" && e.target.value !== "") {
+      this.setState({ tags: [...this.state.tags, e.target.value] });
+      e.target.value = "";
+    }
+  };
+
+  removeTags = (index) => {
+    this.setState({
+      tags: [
+        ...this.state.tags.filter(
+          (tag) => this.state.tags.indexOf(tag) !== index
+        ),
+      ],
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h1>This is the homepage when logged in.</h1>
-      </div>
+      <>
+        <Router>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <>
+                <Header user={this.state.userInfo.firstname} />
+                <Sidebar
+                  page="main"
+                  entries={this.state.entries}
+                  journals={this.state.journals}
+                  tags={this.state.tags}
+                  addTags={this.addTags}
+                  removeTags={this.deleteTags}
+                  handleFormChange={this.handleFormChange}
+                  clearFilters={this.clearFilters}
+                />
+                <MainDisplay
+                  {...props}
+                  page="main"
+                  entries={this.state.entries}
+                  journals={this.state.journals}
+                  journal={this.state.journal}
+                  month={this.state.month}
+                  year={this.state.year}
+                  tags={this.state.tags}
+                />
+              </>
+            )}
+          />
+        </Router>
+      </>
     );
   }
 }
