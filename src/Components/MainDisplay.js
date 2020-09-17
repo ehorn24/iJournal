dfccd853138d5e3bab5623c3898493db2fead94b
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-17 18:04:03 -0500

    add journal page for maindisplay

--- src/Components/MainDisplay.js
+++ src/Components/MainDisplay.js
@@ -10,6 +10,7 @@ const MainDisplay = ({
   month,
   year,
   tags,
+  match,
 }) => {
   //Get user's selected filters, if any
   let currentFilters = {};
@@ -47,32 +48,30 @@ const MainDisplay = ({
   };
   modifyEntries(entries);
 
+  //Filter entries
+  getFilters(journal, month, year, tags);
+  const mEntries = entriesArr.filter((entry) => {
+    return Object.keys(currentFilters).every((k) => {
+      let bool = true;
+      if (typeof entry[k] === "string") {
+        bool = (entry[k] === currentFilters[k]) * bool;
+      } else {
+        let entrySet = new Set(
+          entry.tags.map((t) => {
+            return t;
+          })
+        );
+        let tagsSet = new Set(currentFilters[k]);
+        let intersect = new Set([...entrySet].filter((x) => tagsSet.has(x)));
+        bool = (intersect.size === tagsSet.size) * bool;
+      }
+      return bool;
+    });
+  });
+
   switch (page) {
     //Main page - No journal selected
     case "main":
-      //Filter entries
-      getFilters(journal, month, year, tags);
-      const mEntries = entriesArr.filter((entry) => {
-        return Object.keys(currentFilters).every((k) => {
-          let bool = true;
-          if (typeof entry[k] === "string") {
-            bool = (entry[k] === currentFilters[k]) * bool;
-          } else {
-            let entrySet = new Set(
-              entry.tags.map((t) => {
-                return t;
-              })
-            );
-            let tagsSet = new Set(currentFilters[k]);
-            let intersect = new Set(
-              [...entrySet].filter((x) => tagsSet.has(x))
-            );
-            bool = (intersect.size === tagsSet.size) * bool;
-          }
-          return bool;
-        });
-      });
-
       //Display all journals if no entries selected
       if (Object.keys(currentFilters).length === 0) {
         return (
@@ -86,7 +85,7 @@ const MainDisplay = ({
                 {journals.map((journal, i) => {
                   return (
                     <Link
-                      to={`/journal/${journal.journal_name}`}
+                      to={`/journal/${journal.id}/${journal.journal_name}`}
                       className="journal"
                       key={i}
                     >
@@ -128,6 +127,81 @@ const MainDisplay = ({
           </main>
         );
       }
+    case "journal":
+      //Get all entries in this journal
+      let journalEntries = entriesArr.filter((ent) => {
+        return ent.journal === match.params.journal_id;
+      });
+      //Display all entries in selected journal if no filters are selected
+      if (Object.keys(currentFilters).length === 0) {
+        return (
+          <main className="maindisplay-main">
+            <section className="my-journals">
+              <h4 className="maindisplay-header">
+                {match.params.journal_name}
+              </h4>
+              <Link to="/new/entry" className="addjournal-button">
+                New +
+              </Link>
+              <div className="maindisplay-entries">
+                {journalEntries.length === 0 ? (
+                  <h3>
+                    This journal doesn't have any entries yet. Click "New" to
+                    start one!
+                  </h3>
+                ) : (
+                  journalEntries.map((x, i) => {
+                    return (
+                      <EntryDisplay
+                        key={i}
+                        title={x.entry_title}
+                        month={x.month}
+                        date={x.date}
+                        year={x.year}
+                        tags={x.tags}
+                      />
+                    );
+                  })
+                )}
+              </div>
+            </section>
+          </main>
+        );
+        //If user has selected filters, display matching entries
+      } else {
+        return (
+          <main className="maindisplay-main">
+            <section className="my-journals">
+              <h4 className="maindisplay-header">
+                Find what you're looking for?
+              </h4>
+              <div className="maindisplay-entries">
+                {mEntries.length !== null && mEntries.length !== 0 ? (
+                  mEntries.map((x, i) => {
+                    if (x.journal === match.params.journal_id) {
+                      return (
+                        <EntryDisplay
+                          key={i}
+                          title={x.entry_title}
+                          month={x.month}
+                          date={x.date}
+                          year={x.year}
+                          tags={x.tags}
+                        />
+                      );
+                    }
+                    return null;
+                  })
+                ) : (
+                  <p>There were no matching entries</p>
+                )}
+              </div>
+            </section>
+          </main>
+        );
+      }
+    default:
+      return <h1>Default case</h1>;
   }
 };
 
