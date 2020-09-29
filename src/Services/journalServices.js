cb7aa36074bda8986a77ffff9306603fe8d852f8
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-28 19:32:51 -0500

    added journal services

--- src/Services/journalServices.js
+++ src/Services/journalServices.js
@@ -10,6 +10,31 @@ const journalServices = {
       },
     }).then((res) => res.json());
   },
+
+  addNewJournal(user_id, journal_name) {
+    return fetch(`${journalURL}/u/${user_id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        journal_name: journal_name,
+      }),
+    }).then((res) => res.json());
+  },
+
+  editJournal(journal_id, journal_name, journal_cover) {
+    return fetch(`${journalURL}/j/${journal_id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        journal_name: journal_name,
+        journal_cover: journal_cover,
+      }),
+    });
+  },
 };
 
 module.exports = journalServices;
