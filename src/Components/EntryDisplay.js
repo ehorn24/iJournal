d403597394e6a9affaab6bbc0492fbad39006d9a
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-28 19:34:10 -0500

    entry display functionality

--- src/Components/EntryDisplay.js
+++ src/Components/EntryDisplay.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-const EntryDisplay = ({ title, month, date, year, tags, openModal }) => {
+const EntryDisplay = ({
+  title,
+  month,
+  date,
+  year,
+  tags,
+  id,
+  openEntryModal,
+}) => {
   return (
-    <div className="entry" onClick={(e) => openModal(e, title)}>
+    <div
+      className="entry"
+      onClick={(e) => {
+        e.preventDefault();
+        openEntryModal(id);
+      }}
+    >
       <h3>{title}</h3>
       <p>
         {month.slice(1, 2)}-{date.slice(1, 2)}-{year}
