9d6b9ec110f17b8bf048afafa61c6ffceffb799e
Ella Horn <ellalouisahorn@yahoo.com>
2020-09-29 19:38:52 -0500

    added api to api url

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 4000,
   API_BASE_URL:
     process.env.REACT_APP_API_BASE_URL ||
-    "https://afternoon-dawn-39780.herokuapp.com/",
+    "https://afternoon-dawn-39780.herokuapp.com/api",
 };
