e8112ab3abd120253c24fb00a9b7e1e79b71746f
Ella Horn <ellalouisahorn@yahoo.com>
2020-10-04 21:35:58 -0400

    new entry css

--- src/Components/NewEntry.js
+++ src/Components/NewEntry.js
@@ -10,38 +10,49 @@ const NewEntry = ({
   removeTags,
 }) => {
   return (
-    <div>
-      <form action="" onChange={handleFormChange}>
+    <div className="new-entry-div">
+      <form className="new-entry-form" onChange={handleFormChange}>
         <div>
           <ul>
             <li>
-              <label htmlFor="">Entry Title:</label>
-              <input type="text" name="newEntryTitle" id="newEntryTitle" />
-            </li>
-            <li>
-              <label htmlFor="">Tags:</label>
-              <TagsInput
-                tags={tags}
-                addTags={addTags}
-                removeTags={removeTags}
-                onKeyPress={(e) => {
-                  e.target.keyCode === 13 && e.preventDefault();
-                }}
+              <label htmlFor=""> Title</label>
+              <input
+                type="text"
+                name="newEntryTitle"
+                id="newEntryTitle"
+                className="new-entry-input"
               />
             </li>
             <li>
-              <label htmlFor="">Date:</label>
+              <label htmlFor="">Date</label>
               <input
                 type="text"
                 name=""
                 id=""
                 readOnly="readonly"
                 placeholder={new Date().toDateString()}
+                className="new-entry-input"
               />
             </li>
             <li>
-              <label htmlFor="">Pour your heart out...</label>
+              <label htmlFor="">Tags</label>
+              <div className="new-entry-tags">
+                <TagsInput
+                  tags={tags}
+                  addTags={addTags}
+                  removeTags={removeTags}
+                  onKeyPress={(e) => {
+                    e.target.keyCode === 13 && e.preventDefault();
+                  }}
+                />
+              </div>
+            </li>
+            <li>
+              <label className="new-entry-text-label" htmlFor="">
+                Pour your heart out...
+              </label>
               <textarea
+                className="new-entry-textarea"
                 name="newEntryText"
                 id="newEntryText"
                 cols="30"
@@ -52,6 +63,7 @@ const NewEntry = ({
               ></textarea>
             </li>
             <button
+              className="add-entry-button"
               type="button"
               onClick={(e) => {
                 e.preventDefault();
